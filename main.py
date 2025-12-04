9274067036b80498737cb1eaac0f3d31e56469a3
monikafin <monika.barfa@finaurt.com>
2025-12-05 04:30:35 +0530

    updating requierment-with-main1.txt

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 # -----------------------------
 load_dotenv()
 API_TOKEN = os.getenv("API_TOKEN") # replace with your token
-ZOHO_FLOW_URL =  os.getenv("ZOHO_FLOW_URL") # replace with your Zoho Flow URL
+ZOHO_FLOW_URL =  os.getenv("ZOHO_WEBHOOK_URL") # replace with your Zoho Flow URL
 
 # -----------------------------
 # LOGGING SETUP
